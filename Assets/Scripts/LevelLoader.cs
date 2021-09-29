3fd9591b9945243e15f811d66e3b6ea060fdc808
axelbadde <81579765+axelbadde@users.noreply.github.com>
2021-09-29 20:58:55 +0200

    Scenes order, wall slide and other stuff

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+    public bool useIntegerToLoadLevel = false;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        GameObject collisonGameObject = collison.gameObject;
+
+        if (collisonGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if(useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+}
